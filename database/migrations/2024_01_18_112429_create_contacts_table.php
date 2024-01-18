546bcf6a33ce57abdee167d062edd6d8bdf47453
Moon <kwarnkham.moon@gmail.com>
2024-01-18 22:35:35 +0630

    fix migration

--- database/migrations/2024_01_18_112429_create_contacts_table.php
+++ database/migrations/2024_01_18_112429_create_contacts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('phone');
-            $table->string('message');
+            $table->string('message')->nullable();
             $table->timestamps();
         });
     }
